4709732a49e143ab5ea00f0c526d156590b7c98a
Robbie Bart <adm@AdminisatorsAir.lan>
2020-06-23 13:16:49 -0400

    q1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -2,13 +2,30 @@
 // ----------
 
 let mostPopularFood = [
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
   undefined,
 ];
+
+const fixList = (arr, first, last) => {
+  arr.pop();
+  arr.unshift(first);
+  arr.push(last);
+  return arr;
+};
+
+const printList = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${i + 1}. ${arr[i]}`);
+  }
+};
+
+printList(
+  fixList(mostPopularFood, "bean burritos", "buffalo-flavored cauliflower")
+);
