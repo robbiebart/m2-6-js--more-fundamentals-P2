b0568f3687ea2242410f18da65e7da5e71aa9c88
Robbie Bart <adm@AdminisatorsAir.lan>
2020-06-23 13:33:03 -0400

    q2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,11 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+const printGuestOrder = (early, late) => {
+    let log = early.concat(late);
+    for (let i = 0; i < log.length; i++) {
+        console.log(`${i +1}. ${log[i]}`)
+    }
+}
+printGuestOrder(earlyBirds, lateComers);
